199bd0b1f84e251404f1819a8a13149e732cbff4
Pablo Vega <pablo.vega.gallego@est.fib.upc.edu>
2022-01-03 12:58:24 +0100

    Added annamestre apunts

--- sesion6/tomografia.pl
+++ sesion6/tomografia.pl
@@ -32,30 +32,37 @@ p:- ejemplo1(RowSums,ColSums),
     listVars(NVars, L),  % generate a list of Prolog vars (their names do not matter)
     matrixByRows(L, NumCols, MatrixByRows), % Lista de listas
     transpose(MatrixByRows, TransposedMatrix),
-    declareConstraints(L, MatrixByRows, TransposedMatrix, RowSums, ColSums),
+    declareConstraints(MatrixByRows, TransposedMatrix, RowSums, ColSums, L),
     pretty_print(RowSums, ColSums, MatrixByRows).
 
+listVars(NVars, L):- length(L, NVars).
 
-matrixByRows(ListaRecta, NumColumnas, ResultadoMatriz):-
-	length(ListaRecta, SizeListaRecta),
-	NumeroFilas is SizeListaRecta div NumColumnas,
-	length(ResultadoMatriz, NumeroFilas),
-	maplist(comprobarSize(NumColumnas), ResultadoMatriz),
-	append(ResultadoMatriz, ListaRecta).
+% Create a Matrix of NumCols columns from all variables in the list.
+matrixByRows(L, NumCols, MatrixByRows):-
+            length(L, Lsize),
+            NumRows is Lsize div NumCols,
+            length(MatrixByRows, NumRows),
+            maplist(checkLength(NumCols), MatrixByRows), % All rows have length NumCols
+            append(MatrixByRows, L).
 
-declareConstraints(ListaRecta, Matriz, Transpuesta, SumaFilas, SumaCols):-
-	ListaRecta ins 0..1,
-	checkSum(Matriz, SumaFilas),
-	checkSum(Transpuesta, SumaCols),
-	label(ListaRecta).
+% maplist(:Goal, ?List)
+% True if Goal can successfully be applied on all elements of List.
+% Arguments are reordered to gain performance as well as to make the predicate deterministic under normal circumstances.
+checkLength(Length, List):- length(List, Length).
 
-checkSum([],[]):- !.
-checkSum([L|Matriz], [Result|Resto]):- sum(L, #=, Result), checkSum(Matriz, Resto).
+declareConstraints(Mrows, Mtransp, Rsums, Csums, L):-
+                   L ins 0..1,
+                   checkSum(Mrows, Rsums),
+                   checkSum(Mtransp, Csums),
+                   label(L).
 
-comprobarSize(Tamano, Lista):- length(Lista, Tamano).
+checkSum([], []):- !.
+checkSum([R|Matrix], [S|Sum]):- sum(R, #=, S), checkSum(Matrix, Sum).
+% sum(+Vars, +Rel, ?Expr)
+% The sum of elements of the list Vars is in relation Rel to Expr. Rel is one of #=, #\=, #<, #>, #=< or #>=.
 
 pretty_print(_,ColSums,_):- write('     '), member(S,ColSums), writef('%2r ',[S]), fail.
 pretty_print(RowSums,_,M):- nl,nth1(N,M,Row), nth1(N,RowSums,S), nl, writef('%3r   ',[S]), member(B,Row), wbit(B), fail.
-pretty_print(_,_,_):- nl.
+pretty_print(_,_,_).
 wbit(1):- write('*  '),!.
 wbit(0):- write('   '),!.
